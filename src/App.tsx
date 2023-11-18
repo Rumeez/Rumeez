00e2909642e77931a9143b0aee069163f3d2d7f4
shxwn21 <sigrxnt30@g.ucla.edu>
2023-11-17 18:44:32 -0800

    In progress fix of component organization and login.

--- src/App.tsx
+++ src/App.tsx
@@ -45,30 +45,86 @@
 
 // App.tsx
 
-import React from 'react';
+import React, {useState, ChangeEvent} from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import UserInfo from './userInfo'; // Adjust the import here
 import { handleButtonClick } from './buttonLogic';
+import InputField from './InputField'; // Correct the path if needed
 
-function Home() {
+
+function Login(setLogin_Status, setPassword_status) {
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+  
+    switch (name) {
+      case 'username':
+        setLogin_Status(value);
+          break; 
+      case 'password':
+        setPassword_status(value);
+          break; 
+      default:
+        break;
+    }
+  };
+
+  let temp = "param_req"
+
+  return(
+    <div>
+    <h1>Tell us about yourself.</h1>
+      <InputField label="Username: " name="username" value={temp} onChange={handleInputChange} placeholder="Enter your name" />
+    </div>
+  );
+}
+
+function Createacc() {
+return( 
+  <Link to="/userInfo">
+    <button className="create_acc"> Create Account </button>; 
+  </Link>
+);
+}
+
+
+function Home(setLogin_Status, setPassword_status) { //, accessLogin_status) {
   return (
     <div>
      <h1>Rumeez</h1>
-     <button onClick={handleButtonClick}>Log in</button>
+      <Createacc />
      <h2>Find your perfect roommate.</h2>
-     <button onClick={handleButtonClick}>Create Account</button>
+      <Login setLogin_Status={setLogin_Status} setPassword_status={setPassword_status} />
     <p><Link to="/userInfo">Go to User Info</Link></p>
     </div>
   );
 }
 
 function App() {
+    //Stores user's username
+  const [login_status, setLogin_status] = useState('');
+    //Stores user's password associated with account
+  const [password_status, setPassword_status] = useState('');
+    //Tracks click status of the login button
+  //const [login_click, setLogin_click] = useState(false);
+  
+    //Mutator Functions
+  function changeLogin_status(value: string) { setLogin_status(value)};
+  function changePassword_status(value: string) { setPassword_status(value)};
+    //Accessor Function
+  //function retLogin_Status():string { return login_status}
+
   return (
     <Router>
       <div className="App">
         {/* Use Routes to wrap your Route components */}
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={
+              <Home 
+              setLogin_Status={changeLogin_status} 
+              setPassword_Status={changePassword_status} 
+              //accessLogin_status={retLogin_Status}
+              />} />
           <Route path="/userInfo" element={<UserInfo />} />
           {/* Other routes go here */}
         </Routes>
