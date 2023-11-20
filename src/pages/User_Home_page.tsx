da8da66cd27498a0188d86a4a8bb387a54bbcb27
shxwn21 <sigrxnt30@g.ucla.edu>
2023-11-19 18:18:16 -0800

    Login button linked to User Home Page once verify

---
+++ src/pages/User_Home_page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export function User_Home_Page(): JSX.Element {
+  return(
+    <div> 
+      <p> Logged in :p! User Home Page</p>
+    </div>
+  )
+}
+
+/*
+<Link to="/userInfo">
+    <button onClick={onClick}>
+      {children}
+    </button>
+  </Link>
+*/ 
