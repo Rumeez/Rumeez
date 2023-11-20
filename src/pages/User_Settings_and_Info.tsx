3032b4f709cad2071fcfc77bf35307f91bbd7071
shxwn21 <sigrxnt30@g.ucla.edu>
2023-11-20 11:50:57 -0800

    Adding User_Settings_and_Info file to repo again b/c of git issues

---
+++ src/pages/User_Settings_and_Info.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+
+
+
+const Back_toUser_Home_Page: React.FC<User_Settings_and_Info_Props> = ({handleGoBack}) => {
+    return (
+         <button onClick={handleGoBack}> User Home Page </button>
+    ); 
+}
+
+interface User_Settings_and_Info_Props {
+    handleGoBack: () => void
+}
+
+const User_Settings_and_Info: React.FC<User_Settings_and_Info_Props> = ({handleGoBack}) => {
+    
+    return (
+        <div>
+            <h1> User Settings Page</h1>
+
+            <Back_toUser_Home_Page 
+                handleGoBack={handleGoBack}
+            />
+        </div>
+    );
+}
+
+export default User_Settings_and_Info; 
